53917e7b47eab7e83cf5b0151ea5e7b7696ef638
JavaToster <kamil.gizatullin.03@gmail.com>
2025-08-09 19:19:17 +0300

    change application.yml(use placeholders) and add new field for Authentication DataDTO(forgot username field)

--- src/main/java/com/example/authentication/DTO/auth/AuthenticationDataDTO.java
+++ src/main/java/com/example/authentication/DTO/auth/AuthenticationDataDTO.java
@@ -9,6 +9,7 @@ import lombok.Data;
 public class AuthenticationDataDTO {
     @Min(value = 1, message = "Id should be from 1")
     private long telegramId;
+    private String username;
     @NotBlank(message = "Password should be not empty")
     private String password;
     private String recoveryEmail;
